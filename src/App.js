9bcb2aa66a2695c9d660eb853ea523084888b5e3
Nastya Boiko <boiko.nastasiia@gmail.com>
2022-02-19 20:35:22 +0400

    route mess

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, {useEffect, useState} from 'react';
 import './styles/App.scss';
-import {BrowserRouter} from "react-router-dom";
-import Navbar from "./components/UI/Navbar/Navbar";
-import AppRouter from "./components/AppRouter";
-import {AuthContext} from "./context";
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 
 export default function App() {
   return (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/homepage' element={<Homepage />}>
+          <Route path="clients" element={<Clients />}>
+              <Route path=":id" element={<Client />} />
+              <Route path="new" element={<NewTeamForm />} />
+              <Route index element={<LeagueStandings />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
 
-     
     )
 }
 
