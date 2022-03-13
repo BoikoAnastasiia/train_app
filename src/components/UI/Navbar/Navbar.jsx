3daa83af61aa193c27161ddd24ad317c843492dd
Nastya Boiko <boiko.nastasiia@gmail.com>
2022-03-13 20:02:48 +0400

    links dont work

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,18 +1,32 @@
-import {Link} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import MyButton from "../button";
-import styles from './Navbar.module.scss'
+import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
-    return (
-      <ul className={styles.links}>
-        <li>
-          <Link to="/clients">Клиенты</Link>
-        </li>
-        <li>
-          <Link to="/database">Тренировки</Link>
-        </li>
-      </ul>
-    );
+  return (
+    <ul className={styles.links}>
+      <li>
+        <NavLink
+          to="/clients"
+          className={(isActive) =>
+            styles.navLink + (!isActive ? ` ${styles.unselected}`  : "")
+          }
+        >
+          Клиенты
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/database"
+          className={(isActive) =>
+            styles.navLink + (!isActive ? ` ${styles.unselected}` : "")
+          }
+        >
+          Тренировки
+        </NavLink>
+      </li>
+    </ul>
+  );
 };
 
 export default Navbar;
--- src/pages/DataBase/DataBase.jsx
+++ src/pages/DataBase/DataBase.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from './DataBase.module.scss';
+import Navbar from '../../components/UI/Navbar'
  
 
 export default function DataBase() {
   return (
-    <div>DataBase</div>
+    <>
+    <Navbar/>
+      <div>DataBase</div>
+    </>
   )
 }
