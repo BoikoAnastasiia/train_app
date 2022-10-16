4eedd1c266ea92b0ee252cd1ea6a4cdb3ae6339a
Nastya Boiko <boiko.nastasiia@gmail.com>
2022-10-16 15:38:32 +0400

    added simple component

---
+++ src/components/ClientPreview/ClientPreview.jsx
@@ -0,0 +1,5 @@
+const ClientPreview = ({ avatar, title, trainText, date }) => {
+  return <li>ClientPreview</li>;
+};
+
+export default ClientPreview;
--- src/pages/Clients/Clients.jsx
+++ src/pages/Clients/Clients.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import styles from './Clients.module.scss';
 import Navbar from '../../components/UI/Navbar';
 import FilterInput from '../../components/UI/FilterInput';
+import ClientPreview from '../../components/ClientPreview';
 
 export default function Clients() {
   return (
     <>
       <Navbar />
       <div className={styles.clients_wrapper}>
-        <FilterInput />
-        <button type="button">&#8593;</button>
-        <button type="button">&#8595;</button>
+        <div className={styles.buttons_wrapper}>
+          <FilterInput />
+          <button className={styles.filter_button} type="button">
+            &#8593;
+          </button>
+          <button className={styles.filter_button} type="button">
+            &#8595;
+          </button>
+        </div>
+        <ul className={styles.trains_wrapper}>
+          <ClientPreview avatar="" title="" trainText="" date="" />
+        </ul>
       </div>
     </>
   );
