6edbfb0009a6a7713ac312a9ea022537a65ef829
Nastya Boiko <boiko.nastasiia@gmail.com>
2022-11-05 17:54:33 +0200

    added styles to select

--- src/components/UI/Select/Select.jsx
+++ src/components/UI/Select/Select.jsx
@@ -1,9 +1,17 @@
+import styles from './Select.module.scss';
+
 const Select = ({ options, defaultValue, value, onChange }) => {
   return (
-    <select value={value} onChange={event => onChange(event.target.value)}>
-      <option value="">{defaultValue}</option>
+    <select
+      className={styles.select}
+      value={value}
+      onChange={event => onChange(event.target.value)}
+    >
+      <option className={styles.option} value="">
+        {defaultValue}
+      </option>
       {options.map(option => (
-        <option key={option} value={option}>
+        <option className={styles.option} key={option} value={option}>
           {option}
         </option>
       ))}
