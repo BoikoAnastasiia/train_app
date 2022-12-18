835b500370d8ceccf465603a2159c1b4cca6b2a2
Nastya Boiko <boiko.nastasiia@gmail.com>
2022-12-18 17:33:26 +0100

    works

--- src/pages/Clients/Clients.jsx
+++ src/pages/Clients/Clients.jsx
@@ -10,19 +10,20 @@ import img3 from '../../images/avatars/cat_4.svg';
 import MyModal from '../../components/UI/MyModal/MyModal';
 
 export default function Clients() {
-  const handleQuery = (e) => setQuery(e.target.value);
   const [modal, setModal] = useState(false);
   const handleBlur = (e) => setQuery('');
   const [query, setQuery] = useState('');
+
   const addHandler = () => setModal(true);
-  const trains = [
+
+  const initialClients = [
     {
       avatar: img,
       title: 'Руслан',
       id: Math.random() + Date.now(),
       trainText:
         'Болгарские выпады 10 1 6*2 10 4 тяга в кроссовере бирюзовая 45 12 4 Ягодичный мостик на одной ноге 12 4 сгибание ног в тренажере и еще че тоjhvmjvvjbkbcksahbwflekelcvfewve',
-      date: new Date('October 13, 2023 11:13').toDateString()
+      date: new Date('October 13, 2023 11:13').toDateString(),
     },
     {
       avatar: img2,
@@ -30,7 +31,7 @@ export default function Clients() {
       id: Math.random() + Date.now(),
       trainText:
         'тяга в кроссовере бирюзовая 45 12 4 Ягодичный мостик на одной ноге 12 4 сгибание ног в тренажёре 20 12 4 разведение ног в тренажёре 45 15 4 наружная ротация лёжа на боку 16 4 тяга верхнего блока широким хватом 15 12 1 25 12 4 у-подьем рук 10 4 планка упор лёжа динамическая',
-      date: new Date('October 14, 2023 11:13').toDateString()
+      date: new Date('October 14, 2023 11:13').toDateString(),
     },
     {
       avatar: img3,
@@ -38,9 +39,15 @@ export default function Clients() {
       id: Math.random() + Date.now(),
       trainText:
         'Ягодичный мостик на одной ноге 12 4 сгибание ног в тренажёре 20 12 4 разведение ног в тренажёре 45 15 4 наружная ротация лёжа на боку 16 4 тяга верхнего блока широким хватом 15 12 1 25 12 4 у-подьем рук 10 4 планка упор лёжа динамическая тяга в кроссовере бирюзовая 45 12 4 ',
-      date: new Date('September 14, 2023 11:13').toDateString()
-    }
+      date: new Date('September 14, 2023 11:13').toDateString(),
+    },
   ];
+  const [clients, setClients] = useState(initialClients);
+  const handleQuery = (e) => setQuery(e.target.value);
+  const filteredClients = clients.filter((client) =>
+    client.title.toLowerCase().includes(query)
+  );
+  console.log(filteredClients);
   return (
     <>
       <Navbar />
@@ -60,22 +67,15 @@ export default function Clients() {
           </button>
         </div>
         <ul className={styles.trains_wrapper}>
-          {trains &&
-            trains
-              // .filter(el => {
-              //   console.log(el.title);
-              //   console.log(query);
-              //   el.title.in8cludes(query);
-              // })
-              .map((train) => (
-                <ClientPreview
-                  key={train.id}
-                  avatar={train.avatar}
-                  title={train.title}
-                  trainText={train.trainText}
-                  date={train.date}
-                />
-              ))}
+          {filteredClients.map((train) => (
+            <ClientPreview
+              key={train.id}
+              avatar={train.avatar}
+              title={train.title}
+              trainText={train.trainText}
+              date={train.date}
+            />
+          ))}
         </ul>
       </div>
       <MyModal
